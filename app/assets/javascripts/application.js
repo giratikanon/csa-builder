515c606e09b3a41b753b2e6a6f2df3f2577c707c
Tom Giratikanon <giratikanon@Toms-MacBook-Air.local>
2012-04-21 13:55:29 -0400

    CartoDB map works on index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,35 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require_tree .
-//= require cartodb-leaflet
-//= require wax.leaflet.min
+//= require_tree 
 
 $(document).ready(function(){
 
-	var mapbox, cartodb;
-
-	  function initialize() {
-        var map = new L.Map('map_canvas').setView(new L.LatLng(40.675573,-73.96212), 11);
-
-
-        var mapboxUrl = 'http://{s}.tiles.mapbox.com/v3/csabuilder.map-m2jqpyy8/{z}/{x}/{y}.png',
-            mapbox = new L.TileLayer(mapboxUrl, {maxZoom: 17});
-        map.addLayer(mapbox,true);	
-
-	    cartodb = new L.CartoDBLayer({
-	      map_canvas: 'map_canvas',
-	      map: map,
-	      user_name:'csabuilder',
-	      table_name: 'user_locations',
-	      query: "SELECT cartodb_id,user_id,ST_Transform(ST_Buffer(the_geom_webmercator,300),3857) as the_geom_webmercator FROM user_locations",
-	      tile_style:"#user_locations {polygon-fill:#FF1A00;polygon-opacity: 0.4;line-opacity:0;line-color: #FFFFFF;}",
-	      infowindow: false,
-	      auto_bound: false,
-	      debug: true
-	    });
-
-	  }
-
+	initialize();
+	
 });
 
---
+++ app/assets/javascripts/initialize.js
@@ -0,0 +1,23 @@
+var mapbox, cartodb;
+
+function initialize() {
+var map = new L.Map('map-canvas').setView(new L.LatLng(40.675573,-73.96212), 11);
+
+
+var mapboxUrl = 'http://{s}.tiles.mapbox.com/v3/csabuilder.map-m2jqpyy8/{z}/{x}/{y}.png',
+    mapbox = new L.TileLayer(mapboxUrl, {maxZoom: 17});
+map.addLayer(mapbox,true);	
+
+cartodb = new L.CartoDBLayer({
+  map_canvas: 'map_canvas',
+  map: map,
+  user_name:'csabuilder',
+  table_name: 'user_locations',
+  query: "SELECT cartodb_id,user_id,ST_Transform(ST_Buffer(the_geom_webmercator,300),3857) as the_geom_webmercator FROM user_locations",
+  tile_style:"#user_locations {polygon-fill:#FF1A00;polygon-opacity: 0.4;line-opacity:0;line-color: #FFFFFF;}",
+  infowindow: false,
+  auto_bound: false,
+  debug: true
+});
+
+}
