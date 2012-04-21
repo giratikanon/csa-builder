c60be63c4643c13528516d206ea1bf4cb27f529c
Tom Giratikanon <giratikanon@Toms-MacBook-Air.local>
2012-04-21 17:34:08 -0400

    Add right icon shadow image

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -58,7 +58,9 @@ function addMarker(type) {
 
 	var Icon = L.Icon.extend({
 	    iconUrl: '/assets/' + type + '-basic.png',
+	    shadowURL: '/assets/icon-shadow.png',
 	    iconSize: new L.Point(32, 37),
+	    shadowSize: new L.Point(51, 37),
 	    iconAnchor: new L.Point(16, 37),
 	    popupAnchor: new L.Point(-3, -76)
 	});
